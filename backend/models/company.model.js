65d5a2ecae918144d0f0884f10b867d9bb7381c6
Anand kumar <anandkumar01.2020@gmail.com>
2024-12-25 22:17:44 +0530

    updated

--- backend/models/company.model.js
+++ backend/models/company.model.js
@@ -24,4 +24,4 @@ const companySchema = new mongoose.Schema({
   }
 },{timestamps:true})
 
-export const Company = mongoose.model("company" , companySchema);
+export const Company = mongoose.model("Company" , companySchema);
